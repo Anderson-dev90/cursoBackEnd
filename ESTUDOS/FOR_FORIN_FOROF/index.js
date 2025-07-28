fc592d855f0097587ec8c89303038efe40cf664b
Anderson C Silva <andersoncsilvadev@gmail.com>
2025-07-28 06:15:36 -0300

    Mais estudos e exercicios

---
+++ ESTUDOS/FOR_FORIN_FOROF/index.js
@@ -0,0 +1,27 @@
+// For in -> lê os indces ou chaves do objeto
+
+//const frutas = [`Banana`,`Pera`,`Uva`];
+//
+//for(let i in frutas){
+//    console.log(i )
+//}
+
+//const pessoa = {
+//    nome : 'Anderson',
+//    sobrenome : 'Silva',
+//    idade : 35
+//};
+//
+//for(let chaves in pessoa){
+//    console.log(chaves)
+//}
+
+//*******************************************************************//
+
+//FOR OF
+
+const nome = 'Anderson C. Silva';
+
+for(let valor of nome){
+    console.log(valor);
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+/*for( i = 0; i <= 10; i++){
+    if(i % 2 === 0){
+console.log(`${i} é par`)
+    } else {
+        console.log(`${i} é impar`)
+    }
+}*/
+
+
+const listaGames = ['Fortnite','Crash','GTA'];
+
+for(let i = 0; i < listaGames.length; i++){
+    console.log(`indice ${i}`, listaGames[i]);
+}
